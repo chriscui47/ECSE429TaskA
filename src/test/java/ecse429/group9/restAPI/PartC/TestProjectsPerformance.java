e978b91155ff8d7441b0067ae01d5c2e44cecb97
CAMPUS\ccui10 <chriscui47@yahoo.com>
2021-04-01 21:21:20 -0400

    fsd

--- src/test/java/ecse429/group9/restAPI/PartC/TestProjectsPerformance.java
+++ src/test/java/ecse429/group9/restAPI/PartC/TestProjectsPerformance.java
@@ -4,42 +4,31 @@ import ecse429.group9.restAPI.APIInstance;
 import ecse429.group9.restAPI.StatusCodes;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
-
+import org.junit.runners.MethodSorters;
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestProjectsPerformance {
 
-
-    @After
+@Before
     public void killInstance() {
         APIInstance.killInstance();
     }
-
-    @Before
+@After
     public void startInstance()  throws IOException{
         APIInstance.runApplication();
         assertEquals(StatusCodes.SC_SUCCESS, APIInstance.getStatusCode(""));
     }
 
-
-    //GET Projects
-    @Test
-    public void Get_Proj_Status() throws IOException {
-        String project_url = "/projects";
-        assertEquals(StatusCodes.SC_SUCCESS, APIInstance.getStatusCode(project_url));
-    }
-
     //POST Projects
-    @Test
-    public void Create_Valid_Projects() throws IOException, InterruptedException {
+    public static void Create_Valid_Projects(String title, String description) throws IOException, InterruptedException {
         String validID = "/projects";
-        String title = "TitleTest";
-        String description = "DescriptionTest";
 
         JSONObject json = new JSONObject();
         json.put("title", title);
@@ -49,46 +38,35 @@ public class TestProjectsPerformance {
 
         JSONObject response = APIInstance.request("GET", "/projects");
         System.out.println(response);
-        assertEquals(2,response.getJSONArray("projects").length());
     }
 
 
-    @Test
-    public void Update_Description() throws IOException, InterruptedException {
+    public void Update_Description(String description, JSONObject jsonOriginal) throws IOException, InterruptedException {
         String validID = "/projects/1";
-        String description = "testdescription";
 
-        JSONObject json = new JSONObject();
-        json.put("description", description);
-        APIInstance.post(validID,json.toString());
+        JSONObject jsonNew = new JSONObject();
+        jsonNew.put("description", description);
+        APIInstance.post(validID,jsonNew.toString());
         Thread.sleep(500);
 
         JSONObject response = APIInstance.request("GET", "/projects/1");
         assertEquals("testdescription",response.getJSONArray("projects").getJSONObject(0).get("description"));
     }
 
-    @Test
-    public void Update_Completion() throws IOException, InterruptedException {
-        String validID = "/projects/1";
-        boolean completed = true;
-
-        JSONObject json = new JSONObject();
-        json.put("completed", completed);
-        APIInstance.post(validID,json.toString());
-        Thread.sleep(500);
-
-        JSONObject response = APIInstance.request("GET", "/projects/1");
-        assertEquals("true",response.getJSONArray("projects").getJSONObject(0).get("completed"));
-    }
-
     //DELETE Projects/id
-    @Test
     public void Delete_Valid_Projects() throws IOException {
         String valid_id = "/projects/1";
         assertEquals(APIInstance.getStatusCode(valid_id), StatusCodes.SC_SUCCESS);
         APIInstance.request("DELETE",valid_id);
         assertEquals(APIInstance.getStatusCode(valid_id), StatusCodes.SC_NOT_FOUND);
-
     }
 
+@Test
+    public void startTest() throws IOException, InterruptedException {
+
+    for (int i=0; i<100; i++){
+        Create_Valid_Projects("test","yolo");
+    }
+
+    }
 }
